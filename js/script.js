5decc5ea6c2d53ee32d0588a32d6db2e7c5cb259
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-10 18:26:19 +0100

    milestone_4

--- js/script.js
+++ js/script.js
@@ -10,12 +10,25 @@ const cellsTotal = cellsRow * cellsColumn;
 // event listener al click del pulsante play
 playButton.addEventListener("click", function() {
 
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild)
+    };
+
 // Genero le celle
 for (let i = 1; i <= cellsTotal; i++) {
     const newCell = document.createElement("div");
     newCell.classList.add("cell");
+    // inserisco il numero
     newCell.innerText = i;
+    // stampo in pagina
     grid.appendChild(newCell);
+
+    // aggiungo event listener per il click colorando di azzurro con stampa in console
+    newCell.addEventListener("click", function(){
+        newCell.classList.toggle("clicked");
+        console.log(i);
+    });
+
 }
 });
 
